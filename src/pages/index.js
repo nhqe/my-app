ff5c7f9f37ee86119962b7d19f8b22bc45cb635a
Benjamin Hawkyard <benjamin@gathercontent.com>
2018-08-11 11:57:03 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to Design + Code React apps!</h1>
+    <p>Go and build cool stuff using this tutorial!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
