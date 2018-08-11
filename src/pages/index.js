eadabf516aa9e8b6fc51aacb623d3ef11a6b73d0
Benjamin Hawkyard <benjamin@gathercontent.com>
2018-08-11 12:25:03 +0200

    Adding images and structuring index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to Design + Code React apps!</h1>
-    <p>Go and build cool stuff using this tutorial!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to Design + Code React apps!</h1>
+        <p>Go and build cool stuff using this tutorial!</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
